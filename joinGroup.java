fe3324e8d061f8edc3bfe92a76b0153db474ae03
zackismostcertainlycool <unconfigured@null.spigotmc.org>
2026-03-08 18:22:12 -0700

    tried to fix joingroup bugs

--- joinGroup.java
+++ joinGroup.java
@@ -3,35 +3,29 @@
 
 import java.util.Scanner;
 
-private static void initializeGroups() {
+public static void initializeGroups() {
 
-	    groups = new Group[3];
+	    String[] groups = new String[3];
 
-	    groups[0] = new Group(
-	        "Recycling Club",
-	        "Every Saturday at 9 AM",
-	        "recycleclub@gmail.com"
+	    groups[0] = new String(
+	        "Recycling Club \n Every Saturday at 9 AM \n recycleclub@gmail.com",
 	    );
 
-	    groups[1] = new Group(
-	        "Recycling Awareness Group",
-	        "Meetings online at 11 AM every Sunday",
-	        "recyclingAwarenessOfficial@gmail.com"
+	    groups[1] = new String(
+	        "Recycling Awareness Group \n Meetings online at 11 AM every Sunday \n recyclingAwarenessOfficial@gmail.com"
 	    );
 
-	    groups[2] = new Group(
-	        "Sustainability Volunteers",
-	        "Every Wednesday at 6 PM",
-	        "sustainabilityVolunteers@outlook.com"
+	    groups[2] = new String(
+	        "Sustainability Volunteers \n Every Wednesday at 6 PM \n sustainabilityVolunteers@outlook.com"
 	    );
 	}
 
-private static void joinGroup() {
-		System.out.println("\nAvailable Groups\n" + divider);
+public static void joinGroup() {
+		System.out.println("\nAvailable Groups\n" + Main.divider);
 		
 		// Loops through groups and displays group info
 		
-		for(int i = 0; i < groups.length; i++) {
+		for(int i = 0; i < groups[i].length; i++) {
 			System.out.println(i+1 + ". " + groups[i].getName());
 			System.out.println("Meetings: " + groups[i].getMeetings());
 			System.out.println("Contact Info: " + groups[i].getContact() + "\n");
@@ -40,12 +34,12 @@ private static void joinGroup() {
 		System.out.print("Would you like to join a group? (yes/no): ");
 		
 		// Exits method if user types anything but yes
-		String answer = scanner.nextLine().toLowerCase();
+		String answer = Main.scanner.nextLine().toLowerCase();
 		if (!answer.equals("yes")) return;
 		
 		System.out.print("Enter the group number to join: ");
-        int userInput = scanner.nextInt()-1;
-        scanner.nextLine();
+        int userInput = Main.scanner.nextInt()-1;
+        Main.scanner.nextLine();
         
         // Exits method if user types invalid group number
         if(userInput < 0 || userInput >= groups.length) {
