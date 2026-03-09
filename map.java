cc1603cae2136c07ccbf6fa1ed6b0984655d96db
Jacob <jochristy@students.everettcc.edu>
2026-03-08 21:41:55 -0700

    Fixs for Map

--- map.java
+++ map.java
@@ -1,11 +1,13 @@
 // code for Recycling Map
+import java.util.Scanner;
 
 public static void main(String[] args) {
+    Scanner scnr = new Scanner(System.in);
     System.out.println("Recycling Map\n" + Main.divider);
     
     char locationCheck = 'y';
 
-    char userInput = scnr.nextLine();
+    String userInput = scnr.nextLine();
 
 
 
